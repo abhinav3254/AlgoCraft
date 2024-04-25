c58e7dc6a485eeac5f70795ed85fff8cc80680a6
abhinav3254 <abhinav3254@gmail.com>
2024-04-25 09:18:21 +0530

    assignment question 1 done

---
+++ src/recursion/Assignments.java
@@ -0,0 +1,23 @@
+package recursion;
+
+import java.util.Arrays;
+
+public class Assignments {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        sumOfTriangles2(arr,arr.length-1);
+    }
+
+
+    // question 1 solved
+    // link to problem :- https://www.geeksforgeeks.org/sum-triangle-from-array/
+    private static void sumOfTriangles2(int[] arr,int end) {
+        if (arr.length == 0) return;
+        System.out.println(Arrays.toString(arr));
+        int[] temp = new int[end];
+        for (int i = 0;i<end;i++) {
+            temp[i] = arr[i]+arr[i+1];
+        }
+        sumOfTriangles2(temp,end-1);
+    }
+}
