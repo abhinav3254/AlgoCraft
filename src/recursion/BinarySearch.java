0c761bcc0ee7e6cf21458b8125c1afaab9994da8
abhinav3254 <abhinav3254@gmail.com>
2024-04-25 08:22:32 +0530

    binary search tree implementation using iteration

--- src/recursion/BinarySearch.java
+++ src/recursion/BinarySearch.java
@@ -3,18 +3,18 @@ package recursion;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
-        System.out.println(binarySearch(arr,10,50));
+        System.out.println(binarySearch(arr));
     }
 
 
     // binary search tree implementation using iteration
-    private static int binarySearch(int[] arr,int size,int key) {
+    private static int binarySearch(int[] arr) {
         int start = 0;
-        int end = size-1;
+        int end = 10 -1;
         while (start<=end) {
             int mid = (start+end)/2;
-            if (arr[mid] == key) return mid;
-            else if (arr[mid]>key) {
+            if (arr[mid] == 50) return mid;
+            else if (arr[mid]> 50) {
                 // shift end to left side
                 end = mid - 1;
             } else {
