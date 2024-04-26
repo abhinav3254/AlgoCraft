0f7947475a3dd76812f05dbbb091f0783e8e9d0c
abhinav3254 <abhinav3254@gmail.com>
2024-04-26 09:17:01 +0530

    reverse of number using iteration and recursion

--- src/recursion/LevelOneQuestions.java
+++ src/recursion/LevelOneQuestions.java
@@ -12,6 +12,10 @@ public class LevelOneQuestions {
         System.out.println(sumOfNTo1(5));
 
         System.out.println("sum of numbers are :- "+sumOfNumbers(12345));
+
+        System.out.println("number reverse is :- "+reverseNumber(12345,0));
+
+        System.out.println("reverse using iteration :- "+reverseNumberIteration(12345));
     }
 
     // print numbers from n......1
@@ -46,5 +50,23 @@ public class LevelOneQuestions {
     private static int sumOfNumbers(int n) {
         if (n<10) return n;
         return n%10 + sumOfNumbers(n/10);
+    }
+
+    // method to reverse a number
+    private static int reverseNumber (int n,int sum) {
+        if (n <10) return (sum*10) + n;
+        sum = sum * 10 + (n%10);
+        return reverseNumber(n/10,sum);
+    }
+
+    // reverse number using iteration...
+    private static int reverseNumberIteration(int n) {
+        if (n < 10) return n;
+        int m = 0;
+        while (n > 0) {
+            m = m*10+(n%10);
+            n /= 10;
+        }
+        return m;
     }
 }
