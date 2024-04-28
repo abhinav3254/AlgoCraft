f66069d87d03a951efb9360297327888783a61d5
abhinav3254 <abhinav3254@gmail.com>
2024-04-28 15:11:32 +0530

    creating String Builder in every method call

--- src/recursion/strings/Main.java
+++ src/recursion/strings/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(removeAFromStringIteration("baccad"));
         System.out.println(removeAFromStringRecursion("baccad","",0));
+        System.out.println(removeAFromStringRecursionInBody("dadabaccad",0));
     }
 
 
@@ -23,5 +24,15 @@ public class Main {
         if (index == str.length()) return newString;
         if (str.charAt(index) != 'A' && str.charAt(index) != 'a') newString = newString + str.charAt(index);
         return removeAFromStringRecursion(str,newString,index+1);
+    }
+
+
+    // creating String Builder in every method call and then returning whole string builder in the end....
+    private static StringBuilder removeAFromStringRecursionInBody(String str,int index) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (index == str.length()) return stringBuilder;
+        if (str.charAt(index) != 'A' && str.charAt(index) != 'a') stringBuilder.append(str.charAt(index));
+        stringBuilder.append(removeAFromStringRecursionInBody(str,index+1));
+        return stringBuilder;
     }
 }
