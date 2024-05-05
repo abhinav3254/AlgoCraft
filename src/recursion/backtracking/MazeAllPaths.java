90b3827a9f1a06475613bc7e3046b5b24ac4bf2d
abhinav3254 <abhinav3254@gmail.com>
2024-05-05 19:07:34 +0530

    printing all the paths and steps as well

--- src/recursion/backtracking/MazeAllPaths.java
+++ src/recursion/backtracking/MazeAllPaths.java
@@ -1,5 +1,7 @@
 package recursion.backtracking;
 
+import java.util.Arrays;
+
 public class MazeAllPaths {
     public static void main(String[] args) {
         boolean[][] arr = {
@@ -7,7 +9,8 @@ public class MazeAllPaths {
                 {true,true,true},
                 {true,true,true}
         };
-        allPathsMazeBacktracking(arr,0,0,"");
+//        allPathsMazeBacktracking(arr,0,0,"");
+        allPathsMazeBacktrackingPrint(arr,0,0,"",new int[3][3],1);
     }
 
     // here we can't use all the directions because as you can debug it will stuck in between of DUDU.....
@@ -42,6 +45,31 @@ public class MazeAllPaths {
             // now while coming out we are setting up the changes that we did so that It will not disturb another function
             arr[r][c] = true;
         } else return;
+
+    }
+
+
+    private static void allPathsMazeBacktrackingPrint(boolean[][] arr,int r,int c,String p,int[][] path,int step) {
+        if (r == arr.length-1 && c == arr[0].length-1) {
+            // last step is also a step
+            path[r][c] = step;
+            for(int[] temp:path) {
+                System.out.println(Arrays.toString(temp));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+        if (arr[r][c]) {
+            path[r][c] = step;
+            arr[r][c] = false;
+            if (r<arr.length-1) allPathsMazeBacktrackingPrint(arr, r+1, c, p+'D',path,step+1);
+            if (c<arr[0].length-1) allPathsMazeBacktrackingPrint(arr, r, c+1, p+'R',path,step+1);
+            if (c>0) allPathsMazeBacktrackingPrint(arr, r, c-1, p+'L',path,step+1);
+            if (r>0) allPathsMazeBacktrackingPrint(arr, r-1, c, p+'U',path,step+1);
+            arr[r][c] = true;
+            path[r][c] = 0;
+        }
 
     }
 
