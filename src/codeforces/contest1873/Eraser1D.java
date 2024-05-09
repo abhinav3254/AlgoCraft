bc21d6a5cf66b223829fab781c1d45719b5159ec
abhinav3254 <abhinav3254@gmail.com>
2024-05-10 00:56:41 +0530

    solution idea

---
+++ src/codeforces/contest1873/Eraser1D.java
@@ -0,0 +1,15 @@
+package codeforces.contest1873;
+
+public class Eraser1D {
+    // okay I am writing idea how I am going to solve it....
+    /**
+     * input :-
+     *          6 3
+     *          WBWWWB
+     * iterate while B is not found,
+     * now B is found from there we cab take 3 substring then turn all all of them W in on round
+     * the again iterate whike B is not found then If B is found then again repeat
+     *
+     * to optimize this we will check if the String contains B or not
+     */
+}
