df1a8b16f3f0a329431ef091526025a9592369ca
abhinav3254 <abhinav3254@gmail.com>
2024-05-10 21:50:32 +0530

    TLE

--- src/codeforces/contest1873/Eraser1D.java
+++ src/codeforces/contest1873/Eraser1D.java
@@ -16,27 +16,26 @@ public class Eraser1D {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while (t -- >0) {
-			int length = sc.nextInt();
-			int k = sc.nextInt();
-			sc.nextLine();
-			String string  = sc.nextLine();
-			System.out.println(changeAllWhites(string, length, k));
-		}
+        int t = sc.nextInt();
+        while (t -- >0) {
+            int length = sc.nextInt();
+            int k = sc.nextInt();
+            sc.nextLine();
+            String string  = sc.nextLine();
+            System.out.println(changeAllWhites(string, length, k));
+        }
     }
-
     // Here issue is TLE
     private static int changeAllWhites(String p,int remaining,int k) {
-		if (p.isEmpty()) return 0;
-		if (remaining<k && p.charAt(0) == 'B') {
-			return 1;
-		} else {
-			if (p.charAt(0) == 'B') {
-				return 1 + changeAllWhites(p.substring(k), remaining-k, k);
-			} else {
-				return changeAllWhites(p.substring(1), remaining-1, k);
-			}
-		}
-	}
+        if (p.isEmpty()) return 0;
+        if (remaining<k && p.charAt(0) == 'B') {
+            return 1;
+        } else {
+            if (p.charAt(0) == 'B') {
+                return 1 + changeAllWhites(p.substring(k), remaining-k, k);
+            } else {
+                return changeAllWhites(p.substring(1), remaining-1, k);
+            }
+        }
+    }
 }
